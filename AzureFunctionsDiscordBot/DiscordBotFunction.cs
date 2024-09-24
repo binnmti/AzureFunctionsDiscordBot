553c7f0b254f9f84fef73d5b5f4a186b6032899d
Bin Matsui <binnmtiisprogrammer@gmail.com>
2024-09-25 07:17:59 +0900

    Change Message

--- AzureFunctionsDiscordBot/DiscordBotFunction.cs
+++ AzureFunctionsDiscordBot/DiscordBotFunction.cs
@@ -30,17 +30,8 @@ public static class DiscordBotFunction
         ILogger log)
     {
         log.LogInformation("C# HTTP trigger function processed a request.");
-
-        string name = req.Query["name"];
-        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-        dynamic data = JsonConvert.DeserializeObject(requestBody);
-        name = name ?? data?.name;
-        string responseMessage = string.IsNullOrEmpty(name)
-            ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
         await RunDiscordBotAsync(log);
-        return new OkObjectResult(responseMessage);
+        return new OkObjectResult("This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.");
     }
 
     public static async Task RunDiscordBotAsync(ILogger log)
